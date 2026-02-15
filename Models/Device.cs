2a062529c273ea678948a39b42524230798fe462
FirestX <rohozin.max@gmail.com>
2026-02-15 21:27:16 +0100

    Add Device model

--- Models/Device.cs
+++ Models/Device.cs
@@ -6,16 +6,19 @@ namespace CheckOut.Models;
 public class Device
 {
 	[PrimaryKey, Identity]
-	public Guid Id { get; set; }
+	public int TeacherId { get; set; }
 	[Column, NotNull]
-	public required string Fingerprint { get; set; }
+	public required string Fingerprint { get; set; } = null!;
 	[Column, NotNull]
-	public required string DeviceStatus { get; set; }
+	public required string DeviceStatus { get; set; } = null!;
+
+	[Association(ThisKey = nameof(TeacherId), OtherKey = nameof(Teacher.Id)), NotNull]
+	public Teacher Teacher { get; set; } = null!;
 
 	[Column, NotNull]
-	public DateTime LastSeen { get; set; }
+	public DateTime LastSeen { get; set; } = DateTime.UtcNow;
 	[Column, NotNull]
-	public DateTime UpdatedAt { get; set; }
+	public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
 	[Column, NotNull]
-	public DateTime CreatedAt { get; set; }
+	public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
 }
