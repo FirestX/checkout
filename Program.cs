de52a176f8e43657f4f8631a51e1e7c97343df14
FirestX <rohozin.max@gmail.com>
2026-02-15 21:27:18 +0100

    Add check-in API endpoint

--- Program.cs
+++ Program.cs
@@ -1,28 +1,89 @@
-using CheckOut.Models.Dtos;
 using CheckOut.Data;
+using CheckOut.Models;
+using CheckOut.Models.Dtos;
+using LinqToDB;
+using LinqToDB.Async;
 using Microsoft.AspNetCore.Mvc;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddOpenApi();
+builder.Services.AddSwaggerGen();
 
 // Register AppDataContext with SQLite
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection")!;
-builder.Services.AddScoped(_ => new AppDataContext(connectionString));
+var dataOptions = new DataOptions()
+	.UseSQLite(connectionString);
+
+builder.Services.AddScoped(_ => new AppDataContext(dataOptions));
 
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+	app.MapOpenApi();
+
+	app.UseSwagger();
+	app.UseSwaggerUI();
+
+	// Initialize database tables
+	AppDataContext.InitializeDatabase(dataOptions, connectionString);
 }
 
 if (!app.Environment.IsDevelopment())
 {
-    app.UseHttpsRedirection();
+	app.UseHttpsRedirection();
 }
 
-// app.MapPost("/api/check-in", async ([FromBody] TeacherDto teacher, [FromBody] DeviceDto device) => {
-// 		});
+app.MapPost("/api/check-in", async ([FromBody] CheckinReqest reqest, AppDataContext db) =>
+{
+	var teacher = await db.Teachers.FirstOrDefaultAsync(t => t.Email == reqest.Teacher.Email);
+	var device = await db.Devices.FirstOrDefaultAsync(d => d.Fingerprint == reqest.Device.Fingerprint);
+
+	if (teacher is not null && device is not null)
+	{
+		var checkIn = new CheckIn {
+			TeacherId = teacher.Id,
+			DeviceId = device.TeacherId,
+		};
+		await db.InsertAsync(checkIn);
+		return Results.Ok("Check-in successful.");
+	}
+
+	if (teacher is null)
+	{
+		var newTeacher = new Teacher()
+		{
+			GoogleId = reqest.Teacher.GoogleId,
+			Email = reqest.Teacher.Email,
+			FullName = reqest.Teacher.FullName
+		};
+		var teacherId = await db.InsertWithInt32IdentityAsync(newTeacher);
+		var newDevice = new Device()
+		{
+			TeacherId = teacherId,
+			Fingerprint = reqest.Device.Fingerprint,
+			DeviceStatus = DeviceStatus.Pending
+		};
+		var deviceId = await db.InsertWithInt32IdentityAsync(newDevice);
+		Console.WriteLine("Require admin approval for new device.");
+		return Results.Ok("Check-in successful. Your device is pending approval.");
+	}
+
+	if (device is null)
+	{
+		var newDevice = new Device()
+		{
+			TeacherId = teacher.Id,
+			Fingerprint = reqest.Device.Fingerprint,
+			DeviceStatus = DeviceStatus.Pending
+		};
+		var deviceId = await db.InsertWithInt32IdentityAsync(newDevice);
+		Console.WriteLine("Require admin approval for new device.");
+		return Results.Ok("Check-in successful. Your device is pending approval.");
+	}
+
+	return Results.BadRequest("Check-in failed. Please contact support.");
+});
 
 app.Run();
