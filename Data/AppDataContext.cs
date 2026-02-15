46d4a1dcf64b46ff71912bb2b8c3fcb2903d7511
FirestX <rohozin.max@gmail.com>
2026-02-15 21:25:52 +0100

    Add database context initialization

--- Data/AppDataContext.cs
+++ Data/AppDataContext.cs
@@ -1,12 +1,22 @@
 using LinqToDB;
 using CheckOut.Models;
 using LinqToDB.Data;
+using LinqToDB.DataProvider.SQLite;
 
 namespace CheckOut.Data;
 
-public class AppDataContext(string connectionString) : DataConnection(connectionString)
+public class AppDataContext(DataOptions options) : DataConnection(options)
 {
 	public ITable<Teacher> Teachers => this.GetTable<Teacher>();
 	public ITable<Device> Devices => this.GetTable<Device>();
 	public ITable<CheckIn> CheckIns => this.GetTable<CheckIn>();
+
+	public static void InitializeDatabase(DataOptions dataOptions, string connectionString)
+	{
+		SQLiteTools.CreateDatabase(connectionString);
+		using var db = new AppDataContext(dataOptions);
+		db.CreateTable<Teacher>(tableOptions: TableOptions.CreateIfNotExists);
+		db.CreateTable<Device>(tableOptions: TableOptions.CreateIfNotExists);
+		db.CreateTable<CheckIn>(tableOptions: TableOptions.CreateIfNotExists);
+	}
 }
