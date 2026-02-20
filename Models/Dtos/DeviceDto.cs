054d89f94af2ea6047b5fadfe580fbec0e63a92a
Rohozin <rohozin.max@gmail.com>
2026-02-20 12:22:08 +0100

    refactor device dto

--- Models/Dtos/DeviceDto.cs
+++ Models/Dtos/DeviceDto.cs
@@ -2,6 +2,8 @@ namespace CheckOut.Models.Dtos;
 
 public class DeviceDto
 {
-	public required string Fingerprint { get; set; }
-	public required string DeviceStatus { get; set; }
+	public int Id { get; set; }
+	public string Fingerprint { get; set; } = null!;
+	public string DeviceStatus { get; set; } = null!;
+	public DateTime LastSeen { get; set; }
 }
