f0d6e14e657053874e0ea62c87e7343d8cdaa739
Rohozin <rohozin.max@gmail.com>
2026-02-20 11:45:18 +0100

    remove unused dtos

--- Models/Dtos/CheckInRequestDto.cs
+++
@@ -1,6 +0,0 @@
-namespace CheckOut.Models.Dtos;
-
-public class CheckInRequestDto
-{
-	public required string DeviceFingerprint { get; set; }
-}
--- Models/Dtos/CheckinReqest.cs
+++
@@ -1,7 +0,0 @@
-namespace CheckOut.Models.Dtos;
-
-public class CheckinReqest
-{
-	public TeacherDto Teacher { get; set; } = null!;
-	public DeviceDto Device { get; set; } = null!;
-}
