e770c2c6edbd3da17470113e2ef5e59bdbdc0a38
Rohozin <rohozin.max@gmail.com>
2026-02-20 12:22:49 +0100

    add queries to get check ins

--- Services/CheckInService.cs
+++ Services/CheckInService.cs
@@ -1,6 +1,8 @@
 using CheckOut.Data;
 using CheckOut.Models;
+using CheckOut.Models.Dtos;
 using LinqToDB;
+using LinqToDB.Async;
 
 namespace CheckOut.Services;
 
@@ -14,5 +16,58 @@ public class CheckInService(AppDataContext context)
 			DeviceId = deviceId
 		};
 		await context.InsertAsync(checkIn);
+	}
+
+	public async Task<List<CheckInDto>> GetCheckInsWithDetailsAsync()
+	{
+		return await context.CheckIns
+			.LoadWith(c => c.Device)
+			.LoadWith(c => c.Teacher)
+			.Select(c => new CheckInDto
+			{
+				Id = c.Id,
+				Teacher = new TeacherDto
+				{
+					GoogleId = c.Teacher.GoogleId,
+					Email = c.Teacher.Email,
+					FullName = c.Teacher.FullName
+				},
+				Device = new DeviceDto
+				{
+					Id = c.Device.Id,
+					Fingerprint = c.Device.Fingerprint,
+					DeviceStatus = c.Device.DeviceStatus,
+					LastSeen = c.Device.LastSeen
+				},
+				CheckInTime = c.CheckInTime
+			})
+			.ToListAsync();
+	}
+
+	public async Task<List<CheckInDto>> GetCheckInsWithDetailsAsync(string deviceStatus)
+	{
+		return await context.CheckIns
+			.LoadWith(c => c.Device)
+			.LoadWith(c => c.Teacher)
+			.Select(c => new CheckInDto
+			{
+				Id = c.Id,
+				Teacher = new TeacherDto
+				{
+					GoogleId = c.Teacher.GoogleId,
+					Email = c.Teacher.Email,
+					FullName = c.Teacher.FullName
+				},
+				Device = new DeviceDto
+				{
+					Id = c.Device.Id,
+					Fingerprint = c.Device.Fingerprint,
+					DeviceStatus = c.Device.DeviceStatus,
+					LastSeen = c.Device.LastSeen
+				},
+				CheckInTime = c.CheckInTime
+			})
+			.Where(c => c.Device.DeviceStatus == deviceStatus)
+			.ToListAsync();
 	}
 }
